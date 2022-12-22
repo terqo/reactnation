01206b51f8421a2c0f73fe49738f8bc84bbe1578
oscarDGAF <oscar@DGAF>
2022-12-22 01:14:15 -0600

    yep2

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,5 +1,6 @@
 import {useRef, useEffect} from 'react'
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const Scene = () => {
 
@@ -16,7 +17,7 @@ const Scene = () => {
         1000
     )
 
-    camera.position.z = 4
+    camera.position.z = 12
     scene.add(camera)
     
     //renderer
@@ -24,15 +25,93 @@ const Scene = () => {
     renderer.setSize(currentMount.clientWidth,currentMount.clientHeight)
     currentMount.appendChild(renderer.domElement)
 
+    //Controls
+    const controls = new OrbitControls(camera, renderer.domElement)
+    //controls.target = new THREE.Vector3(3,3,3)
+    controls.enableDamping = true 
+
+
     //cube
     const cube = new THREE.Mesh(
       new THREE.BoxBufferGeometry(1,1,1),
-      new THREE.MeshBasicMaterial()
+      new THREE.MeshBasicMaterial({
+        color:0xff0000,
+        transparent:true,
+        opacity: 0.3,
+        wireframe: true,
+
+      })
     )
     scene.add(cube)
     console.log('se mostro el objeto')
+
+    //Sphere
+    const textureLoader = new THREE.TextureLoader()
+    const matcap = textureLoader.load('./textures/matcap3.png')
+
+    const geometry = new THREE.SphereGeometry( 0.8, 32, 16 );
+    const material = new THREE.MeshMatcapMaterial( { 
+        matcap : matcap
+     } );
+    const sphere = new THREE.Mesh( geometry, material );
+    scene.add( sphere );
+    sphere.position.y = 1.5
+
+    //torus
+    const geometry1 = new THREE.TorusKnotGeometry( 0.5, 0.18, 100, 16 );
+    const material1 = new THREE.MeshNormalMaterial( { 
+        flatShading: true,
+    } );
+    const torusKnot = new THREE.Mesh( geometry1, material1 );
+    scene.add( torusKnot );
+    //torusKnot.position.set(-2,-0.5,0)
+    //torusKnot.scale.set(2,2,1)
+    torusKnot.position.y = -1.5
+    
+    //Donut xd
+    const textureLoader2 = new THREE.TextureLoader()
+    const matcap2 = textureLoader2.load('./textures/matcap1.png')
+
+    const geometry2 = new THREE.TorusGeometry( 0.65, 0.15, 20, 100 );
+    const material2 = new THREE.MeshMatcapMaterial( { 
+      matcap: matcap2
+    } );
+    const torus = new THREE.Mesh( geometry2, material2 );
+    scene.add( torus );
+    torus.position.x = 2
+
+    //octahedron
+    const textureLoader3 = new THREE.TextureLoader()
+    const map = textureLoader3.load('./grass/basecolor.png')
+    const aoMap = textureLoader3.load('./grass/AO.png')
+    const roughnessMap = textureLoader3.load('./grass/roughness.png')
+    const normalMap = textureLoader3.load('./grass/normal.png')
+    const heightMap = textureLoader3.load('./grass/height.png')
+
+    const geometry3 = new THREE.OctahedronGeometry( 0.65, 2);
+    const material3 = new THREE.MeshStandardMaterial({
+      map: map,
+      aoMap: aoMap,
+      roughnessMap: roughnessMap,
+      normalMap: normalMap,
+      displacementMap: heightMap
+    });
+    const octahedron = new THREE.Mesh( geometry3, material3 );
+    scene.add( octahedron );
+    octahedron.position.x = -2
+
+    //lights
+    const AO = new THREE.AmbientLight('white', 1)
+    scene.add(AO)
+
     //render the scene 
-    renderer.render(scene, camera)
+    const animate = () => {
+      controls.update()
+      renderer.render(scene, camera)
+      requestAnimationFrame(animate)
+
+    }
+    animate()
     
     //clean up scene 
     return () => {
